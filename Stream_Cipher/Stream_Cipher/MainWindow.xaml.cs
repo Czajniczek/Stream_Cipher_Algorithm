63f9d377929d91c21e5c66ef3b14387fe81ff5d1
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-04-26 15:59:51 +0200

    Views added
    Views added

--- Stream_Cipher/Stream_Cipher/MainWindow.xaml.cs
+++ Stream_Cipher/Stream_Cipher/MainWindow.xaml.cs
@@ -15,14 +15,41 @@ using System.Windows.Shapes;
 
 namespace Stream_Cipher
 {
-    /// <summary>
-    /// Interaction logic for MainWindow.xaml
-    /// </summary>
     public partial class MainWindow : Window
     {
+        private readonly StreamCipherService streamCipherService = new StreamCipherService();
+
         public MainWindow()
         {
             InitializeComponent();
+        }
+
+        private void Generate_LFSR(object sender, RoutedEventArgs e)
+        {
+            string seed = LFSR_Seed.Text.ToString();
+            string polynomial = LFSR_Polynomial.Text.ToString();
+
+            LFSR_Output.Text = streamCipherService.LFSR_Generator(seed, polynomial);
+        }
+
+        private void Encode_Synchronous_Stream_Cipher(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decode_Synchronous_Stream_Cipher(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Encode_Ciphertext_Autokey(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Decode_Ciphertext_Autokey(object sender, RoutedEventArgs e)
+        {
+
         }
     }
 }
---
+++ Stream_Cipher/Stream_Cipher/StreamCipherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Stream_Cipher
+{
+    class StreamCipherService
+    {
+        public string LFSR_Generator(string seed, string polynomial)
+        {
+            return "XD";
+        }
+    }
+}
