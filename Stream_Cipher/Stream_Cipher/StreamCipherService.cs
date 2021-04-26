e890534ce9e168446bd870e425f6ce012972a4ee
Gabrielos1 <73485727+Gabrielos1@users.noreply.github.com>
2021-04-26 16:55:23 +0200

    LFSR added
    LFSR added

--- Stream_Cipher/Stream_Cipher/StreamCipherService.cs
+++ Stream_Cipher/Stream_Cipher/StreamCipherService.cs
@@ -10,7 +10,32 @@ namespace Stream_Cipher
     {
         public string LFSR_Generator(string seed, string polynomial)
         {
-            return "XD";
+            string output = "";
+            int counter = 0;
+            string score = "";
+
+            for (int i = 0; i < seed.Length; i++)
+            {
+                counter = 0;
+                score = "";
+                //string newSeed = seed;
+
+                for (int j = 0; j < polynomial.Length; j++)
+                {
+                    if (seed[j] == '1' && polynomial[j] == '1') counter++;
+                }
+
+                if (counter % 2 == 0) score += '0';
+                else score += '1';
+
+                seed = seed.Substring(0, seed.Length - 1);
+                score += seed;
+                seed = score;
+
+                output += score[0];
+            }
+
+            return output;
         }
     }
 }
